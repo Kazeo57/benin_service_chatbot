ae6ab562f2b9cc2dbbde3644bae56262be8e19ac
ALIMAGNIDOKPO Arix <127417709+Arix-ALIMAGNIDOKPO@users.noreply.github.com>
2025-03-07 18:46:26 +0100

    Update api.py

--- api.py
+++ api.py
@@ -1,22 +1,21 @@
-from flask import Flask ,jsonify,request 
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 from main import chat
 
-app=Flask(__name__)
+app = Flask(__name__)
+CORS(app)  # Autorise toutes les origines
 
-@app.route('/',methods=['POST'])
+@app.route('/', methods=['POST'])
 def home():
-    data=request.get_json()
+    data = request.get_json()
     if data and "query" in data:
-        query=data['query']
+        query = data['query']
         return chat(query)
-    return {"error":"Le paramètre 'query' est manquant"},400
+    return {"error": "Le paramètre 'query' est manquant"}, 400
 
-
-@app.route('/health/',methods=['GET'])
+@app.route('/health/', methods=['GET'])
 def health():
     return "API works well"
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(debug=True)
-
